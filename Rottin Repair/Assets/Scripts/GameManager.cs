fbda69025a3fdb2489a1090b139824911a7cb6d1
mvgribbin <mvgribbin@gmail.com>
2020-02-01 21:07:32 +0100

    added more art assets and worked on scripts

--- Rottin Repair/Assets/Scripts/GameManager.cs
+++ Rottin Repair/Assets/Scripts/GameManager.cs
@@ -1,57 +1,48 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using Collections;
+﻿using System.Collections; using System.Collections.Generic; using UnityEngine; using UnityEngine.UI; using Collections;  public class GameManager : MonoBehaviour {     [Header("AssetsConfig")]      [SerializeField]     public List<ZombiePart> imageDictionary;      #region Game Flow      [Header("LevelConfig")]      [SerializeField]     public int[] levelZombieAmounts;      private int currentScore;     private int CurrentScore     {         get => currentScore;         set         {             currentScore = value;             // TO DO: add UI score counter         }     }
+     private int currentLevel;     private int CurrentLevel     {         get => currentLevel;         set         {             currentLevel = value;             // TO DO: when current level above level amount, win game
+        }
+    }      private int currentLevelZombies;     private int CurrentLevelZombies     {         get => currentLevelZombies;         set         {             currentLevelZombies = value;             // TO DO: when above limit for this level, trigger new level
+        }
+    } 
+    private void Start()     {
+        StartGame();
+    }
+     public void StartGame() {         currentScore = 0;         StartLevel(0);     }      public void StartLevel(int levelId) { }      #endregion      #region Button Callbacks
 
-public class GameManager : MonoBehaviour
-{
-    [SerializeField]
-    public int[] levelZombieAmounts;
-
-    [SerializeField]
-    public Image[,] zombieParts = new Image[Const.BODYPARTS, Const.MAXDECAYSTATES];
-
-    [SerializeField]
-    public List<ZombiePart> imageDictionary;
-
-    private List<ZombiePart> inventory;
-
-    private int currentScore;
-
-    public void StartGame() { }
-
-    public void StartLevel(int levelId) { }
-
-    #region Buttons
-
+    // count up score     // play shipping animation and sounds
+    // raise zombie counter for current level
+    // if zombie counter for this level has reached limit:
+    // start next level / win the game
+    // else bring in next zombie
     public void Ship() { }
 
+    // disable all buttons and inventory
+    // trigger salvage mini-game
     public void Salvage() { }
 
-    public void DeletePart() { }
-
     #endregion
 
-    #region Pause
+    #region Zombie Builder     
+    private ZombieSetup currentSetup; 
+    // set current setup to a new random zombie and start in animation
+    public void BringInNextZombie() { }
 
-    public void PauseGame() { }
-
-    public void UnpauseGame() { }
-
-    #endregion
-
-    #region Zombie Builder
-
-    public void BuildZombie(ZombieTypes type) { }
-
+    // if the current setup doesn't already contain the given zombie part:
+    // remove it from inventory and add to setup
     public void AddZombiePart(ZombiePart part) { }
+     // remove given part from current setup
+    public void DeletePartFromTable(ZombiePart partToRemove) { } 
+    #endregion
+     #region Inventory      private List<ZombiePart> inventory; 
+    // remove given part from inventory
+    public void DeletePartFromInventory(ZombiePart partToRemove) { }
 
     #endregion
-
-    #region Inventory
-
+         #region Pause
+         // set time scale to 0 and open big pause popup
+    public void PauseGame() { }
+         // set time scale to 1 and close pause popup
+    public void UnpauseGame() { }
     
-
-    #endregion
-}
+    #endregion 
+} 
--- Rottin Repair/Assets/Scripts/source/Collections.cs
+++ Rottin Repair/Assets/Scripts/source/Collections.cs
@@ -1,83 +1,3 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
-namespace Collections
-{
-    public enum Parts
-    {
-        None,
-        Head,
-        Body,
-        ArmL,
-        ArmR,
-        LegL,
-        LegR
-    }
-
-    public enum ZombieTypes
-    {
-        Nurse
-    }
-
-    public class Const
-    {
-        public const int BODYPARTS = 6;
-
-        public const int MAXDECAYSTATES = 3;
-
-        public const int MAXINVENTORYSPACE = 10;
-    }
-
-    [System.Serializable]
-    public class ZombiePart
-    {
-        public Parts part;
-
-        public ZombieTypes zombieType;
-
-        [Range(0, Const.MAXDECAYSTATES-1)]
-        public int decayState;
-
-        public Image image;
-    }
-
-    [System.Serializable]
-    public class ZombieSetup
-    {
-        public Dictionary<Parts, ZombiePart> parts;
-
-        public int SetupScore {
-            get
-            {
-                int sum = 0;
-                foreach(KeyValuePair<Parts, ZombiePart> pair in parts)
-                {
-                    sum += (Const.MAXDECAYSTATES - pair.Value.decayState);
-                }
-                return sum;
-            }
-        }
-
-        public bool HasPart(Parts part)
-        {
-            return parts.ContainsKey(part);
-        }
-
-        public void RemovePart(Parts partToRemove)
-        {
-            if (parts.ContainsKey(partToRemove))
-            {
-                parts.Remove(partToRemove);
-            }
-        }
-
-        public void AddPart(ZombiePart part)
-        {
-            if(parts.ContainsKey(part.part)) {
-                return;
-            }
-            parts.Add(part.part, part);
-        }
-    }
-}
+﻿using System.Collections.Generic; using UnityEngine; using UnityEngine.UI;  namespace Collections {     public enum Parts     {         None,         Head,         Body,         ArmL,         ArmR,         LegL,         LegR     }      public enum ZombieTypes     {         Nurse     }      public class Const     {         public const int BODYPARTS = 6;          public const int MAXDECAYSTATES = 3;          public const int MAXINVENTORYSPACE = 10;     }      [System.Serializable]     public class ZombiePart     {         public Parts part;          public ZombieTypes zombieType;          [Range(0, Const.MAXDECAYSTATES - 1)]         public int decayState;          public Image image;     }      [System.Serializable]     public class ZombieSetup     {         public Dictionary<Parts, ZombiePart> parts;          public int SetupScore
+        {             get             {                 int sum = 0;                 foreach (KeyValuePair<Parts, ZombiePart> pair in parts)                 {                     sum += (Const.MAXDECAYSTATES - pair.Value.decayState);                 }                 return sum;             }         }          public bool HasPart(Parts part)         {             return parts.ContainsKey(part);         }          public void RemovePart(Parts partToRemove)         {             if (parts.ContainsKey(partToRemove))             {                 parts.Remove(partToRemove);             }         }          public void AddPart(ZombiePart part)         {             if (parts.ContainsKey(part.part))
+            {                 return;             }             parts.Add(part.part, part);         }     }   }  
