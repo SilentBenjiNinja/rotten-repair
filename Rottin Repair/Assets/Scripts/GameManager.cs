914d545820a7e58ccc210fe611f82973a42cfdb8
mvgribbin <mvgribbin@gmail.com>
2020-02-02 01:49:27 +0100

    added sounds; created UI scripts

--- Rottin Repair/Assets/Scripts/GameManager.cs
+++ Rottin Repair/Assets/Scripts/GameManager.cs
@@ -1,48 +1,84 @@
-﻿using System.Collections; using System.Collections.Generic; using UnityEngine; using UnityEngine.UI; using Collections;  public class GameManager : MonoBehaviour {     [Header("AssetsConfig")]      [SerializeField]     public List<ZombiePart> imageDictionary;      #region Game Flow      [Header("LevelConfig")]      [SerializeField]     public int[] levelZombieAmounts;      private int currentScore;     private int CurrentScore     {         get => currentScore;         set         {             currentScore = value;             // TO DO: add UI score counter         }     }
-     private int currentLevel;     private int CurrentLevel     {         get => currentLevel;         set         {             currentLevel = value;             // TO DO: when current level above level amount, win game
+﻿using System.Collections; using System.Collections.Generic; using UnityEngine; using Collections; using System.Linq;  public class GameManager : MonoBehaviour {
+    [SerializeField]     public UIHandler uiHandler;      [Header("AssetsConfig")]      [SerializeField]     public List<ZombiePart> imageDictionary;      #region Game Flow      [Header("LevelConfig")]      [SerializeField]     public int[] levelZombieAmounts;      private int currentScore;     private int CurrentScore     {         get => currentScore;         set         {             currentScore = value;             uiHandler.SetScore(value.ToString());         }     }
+     private int currentLevel;     private int CurrentLevel     {         get => currentLevel;         set         {             currentLevel = value;             if (CurrentLevel >= levelZombieAmounts.Length) {                 EndGame();             }
         }
-    }      private int currentLevelZombies;     private int CurrentLevelZombies     {         get => currentLevelZombies;         set         {             currentLevelZombies = value;             // TO DO: when above limit for this level, trigger new level
-        }
+    }      private int currentLevelZombie;     private int CurrentLevelZombie     {         get => currentLevelZombie;         set         {             currentLevelZombie = value;             if (CurrentLevelZombie >= levelZombieAmounts[CurrentLevel]) {
+                StartLevel(CurrentLevel+1);             }             else
+            {
+                BringInNextZombie();
+            }         }
     } 
     private void Start()     {
         StartGame();
     }
-     public void StartGame() {         currentScore = 0;         StartLevel(0);     }      public void StartLevel(int levelId) { }      #endregion      #region Button Callbacks
+     public void StartGame() {         currentScore = 0;         StartLevel(0);     }      public void StartLevel(int levelId) {}      public void EndGame() { }       private IEnumerator QuickTimeEvent() {         float phaseShift = Random.Range(-Mathf.PI, Mathf.PI);         float frequency = GetFrequency();         float startingTime = Time.time;          bool isClicked = false;         float currentCursorPosition = phaseShift;         while(isClicked == false)         {
+            currentCursorPosition = (float)(0.5 * Mathf.Sin(frequency * (Time.time - startingTime) + phaseShift) + 0.5);             print(currentCursorPosition);             yield return null;
+            if (Input.anyKey)             {
+                isClicked = true;
+            }
+        }
+        AddToInventory(GetPartsForQuicktimeEvent(currentCursorPosition));
+    }      private float GetFrequency()     {         // TO DO: implement frequency depending on zombie value         return 3;
+    }
 
-    // count up score     // play shipping animation and sounds
-    // raise zombie counter for current level
-    // if zombie counter for this level has reached limit:
-    // start next level / win the game
-    // else bring in next zombie
-    public void Ship() { }
+    private List<ZombiePart> GetPartsForQuicktimeEvent(float quicktimeResult)     {         float partSalvagePercentage = 0;
+        int additionalPart = 0;          // TO DO: check current position against dictionary of positions         // green zone         if(quicktimeResult < 0.6 && quicktimeResult > 0.4)         {
+            partSalvagePercentage = 1;
+        }
+        //blue zone         else if(quicktimeResult < 0.8 && quicktimeResult > 0.2)         {
+            partSalvagePercentage = 0.5f;
+        }
+         if (partSalvagePercentage == 0.5f && currentSetup.parts.Count % 2 == 1)         {
+            if (Random.Range(0, 1) > 0.5f) {
+                additionalPart = 1;             }             else             {
+                additionalPart = -1;
+            }
+        }
+        int partsToPickFromZombie = (int)((currentSetup.parts.Count + additionalPart) * partSalvagePercentage);         List<ZombiePart> returnValue = new List<ZombiePart>();          // add random parts from table to inventory according to result         return returnValue;
+    }      #endregion      #region Button Callbacks
+
+    // play shipping animation and sounds
+    public void Ship()
+    {
+        CurrentScore += currentSetup.SetupScore;         CurrentLevelZombie += 1; 
+    }
 
     // disable all buttons and inventory
-    // trigger salvage mini-game
-    public void Salvage() { }
+    public void Salvage()
+    {
+        StartCoroutine(QuickTimeEvent());
+    }
 
+    public void PauseGame() {         Time.timeScale = 0;
+        uiHandler.ShowPauseMenu();     }
+         public void UnpauseGame() {
+        Time.timeScale = 1;         uiHandler.HidePauseMenu();     } 
+    #endregion      #region Zombie Builder     
+    public ZombieSetup currentSetup;      public void BringInNextZombie() {         currentSetup = new ZombieSetup();         currentSetup.parts = new Dictionary<Parts, ZombiePart>();
+        for (int i = 0; i < Const.BODYPARTS; i++)
+        {
+            AddRandomPartToDictionary((Parts)(i+1));
+        }
+        // TO DO: show images; play in animation and sounds     } 
+    public void AddZombiePart(ZombiePart part) {         DeletePartFromInventory(part);         currentSetup.AddPart(part);
+        // TO DO: refresh images     }      public void DeletePartFromTable(Parts partToRemove) {         currentSetup.RemovePart(partToRemove);         // TO DO: refresh images
+    }
+
+    private void AddRandomPartToDictionary(Parts part) {
+        List<ZombiePart> partsList = imageDictionary.Where(zp => zp.part == part) as List<ZombiePart>;         currentSetup.parts.Add(part, partsList[Random.Range(0, partsList.Count)]);
+    } 
     #endregion
 
-    #region Zombie Builder     
-    private ZombieSetup currentSetup; 
-    // set current setup to a new random zombie and start in animation
-    public void BringInNextZombie() { }
+    #region Inventory
 
-    // if the current setup doesn't already contain the given zombie part:
-    // remove it from inventory and add to setup
-    public void AddZombiePart(ZombiePart part) { }
-     // remove given part from current setup
-    public void DeletePartFromTable(ZombiePart partToRemove) { } 
-    #endregion
-     #region Inventory      private List<ZombiePart> inventory; 
-    // remove given part from inventory
-    public void DeletePartFromInventory(ZombiePart partToRemove) { }
+    private List<ZombiePart> inventoryList = new List<ZombiePart>(); 
+    public void DeletePartFromInventory(ZombiePart partToRemove) {         if (inventoryList.Contains(partToRemove))
+        {
+            inventoryList.Remove(partToRemove);
+        }         // TO DO: refresh images     }      public void AddToInventory(List<ZombiePart> partList) {         if(inventoryList.Count + partList.Count <= Const.MAXINVENTORYSPACE)
+        {
+            inventoryList.AddRange(partList);             // TO DO: refresh images
+        }     }
 
     #endregion
-         #region Pause
-         // set time scale to 0 and open big pause popup
-    public void PauseGame() { }
-         // set time scale to 1 and close pause popup
-    public void UnpauseGame() { }
-    
-    #endregion 
 } 
---
+++ Rottin Repair/Assets/Scripts/UIHandler.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Collections;
+
+public class UIHandler : MonoBehaviour
+{
+    public Button btn_salvage;
+    public GameObject txt_cantSalvage;
+    public Text txt_cantSalvageReason;
+
+    public Button btn_ship;
+    public GameObject txt_cantShip;
+    public Text txt_cantShipReason;
+
+    public Text txt_scoreValue;
+
+    public GameObject grp_bottom;
+
+    public GameObject prefab_inventoryItem;
+
+    public GameObject img_pauseMenu;
+
+    private void Awake()
+    {
+        // btn_salvage = this.transform.Find("grp_top").Find("grp_salvage").Find("btn_salvage*").GetComponent<Button>();
+        // txt_cantSalvage = this.transform.Find("grp_top").Find("grp_salvage").Find("txt_cant_salvage*").gameObject;
+        // txt_cantSalvageReason = this.transform.Find("grp_top").Find("grp_salvage").Find("txt_cant_salvage_reason*").GetComponent<Text>();
+
+        // btn_ship = this.transform.Find("grp_top").Find("grp_ship").Find("btn_ship*").GetComponent<Button>();
+        // txt_cantShip = this.transform.Find("grp_top").Find("grp_ship").Find("txt_cant_ship*").gameObject;
+        // txt_cantShipReason = this.transform.Find("grp_top").Find("grp_ship").Find("txt_cant_ship_reason*").GetComponent<Text>();
+
+        // txt_scoreValue = this.transform.Find("grp_top").Find("grp_score").Find("txt_score_value").GetComponent<Text>();
+
+        // grp_bottom = this.transform.Find("grp_bottom").gameObject;
+
+        // img_pauseMenu = this.transform.Find("img_pause_menu").gameObject;
+    }
+
+    private void Start()
+    {
+        HidePauseMenu();
+        EnableShipping();
+        EnableSalvaging();
+        SetScore("0");
+        while(grp_bottom.transform.childCount > 0)
+        {
+            Destroy(grp_bottom.transform.GetChild(0).gameObject);
+        }
+    }
+
+    public void EnableSalvaging() {
+        btn_salvage.interactable = true;
+        txt_cantSalvage.SetActive(false);
+        txt_cantSalvageReason.gameObject.SetActive(false);
+    }
+
+    public void DisableSalvaging(string reason) {
+        btn_salvage.interactable = false;
+        txt_cantSalvage.SetActive(true);
+        txt_cantSalvageReason.gameObject.SetActive(true);
+        txt_cantSalvageReason.text = reason;
+    }
+
+    public void EnableShipping() {
+        btn_ship.interactable = true;
+        txt_cantShip.SetActive(false);
+        txt_cantShipReason.gameObject.SetActive(false);
+    }
+
+    public void DisableShipping(string reason) {
+        btn_ship.interactable = false;
+        txt_cantShip.SetActive(true);
+        txt_cantShipReason.gameObject.SetActive(true);
+        txt_cantShipReason.text = reason;
+    }
+
+    public void SetScore(string newValue) {
+        txt_scoreValue.text = newValue;
+    }
+
+    public void AddInventoryItem(ZombiePart newPart) { }// add button listener
+
+    public void RemoveInventoryItem(int position) { }   // how to determine which one to remove?
+
+    public void ShowPauseMenu() { 
+        img_pauseMenu.SetActive(false);
+    }
+
+    public void HidePauseMenu() {
+        img_pauseMenu.SetActive(false);
+    }
+}
