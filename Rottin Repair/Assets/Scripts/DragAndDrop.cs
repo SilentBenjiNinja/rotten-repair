8668277802f6b1763c24ec8a487a69cabb058a13
CU <bla@bla.de>
2020-02-08 20:13:19 +0100

    added scripts for drag and drop

---
+++ Rottin Repair/Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DragAndDrop : MonoBehaviour
+{
+    public bool isMouseDown;
+
+    public Image[] arr_img_inventory;
+
+    //public Image fromButton;
+    //public Image toButton;
+    public Image currentHoverImage;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            OnMouseDown();
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            OnMouseUp();
+        }
+
+
+    }
+
+    private void OnMouseDown()
+    {
+        if (isMouseDown) return;
+        StartCoroutine(Drag(currentHoverImage));
+    }
+
+    private void OnMouseUp()
+    {
+        if (!isMouseDown) return;
+        StartCoroutine(Drop(currentHoverImage));
+    }
+
+    private IEnumerator Drag(Image currentHoverImage)
+    {
+        yield return new WaitUntil(() => !isMouseDown);
+
+    }
+
+    private IEnumerator Drop(Image currentHoverImage)
+    {
+        yield return new WaitUntil(() => isMouseDown);
+    }
+
+
+}
---
+++ Rottin Repair/Assets/Scripts/Draggable.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        print("begindrag");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        print("drag");
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        print("end drag");
+        if (HasGameObjectWithName(eventData.hovered, "dropzone_table"))
+        {
+            // TO DO: stuff when dropping onto table
+        }
+        else if (HasGameObjectWithName(eventData.hovered, "dropzone_inventory"))
+        {
+            // TO DO: stuff when dropping into inventory
+        }
+    }
+
+    private bool HasGameObjectWithName(List<GameObject> gameObjects, string name)
+    {
+        foreach (GameObject gObj in gameObjects)
+        {
+            if (gObj.name == name)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
